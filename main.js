280c4244c033569fe73c7a72cec34520e4b30924
meganasus <crisgarc06@outlook.com>
2025-06-15 13:14:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-firstH1 = document.querySelector("h1");
+const firstH1 = document.querySelector("h1");
 firstH1.addEventListener("click", () => {
   if (firstH1.classList.includes("yellow") firstH1.classList.remove("yellow");
   else firstH1.classList.add("yellow");
