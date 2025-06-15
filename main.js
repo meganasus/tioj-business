6dd98052c85d78841552880861ac8749c7066c53
meganasus <crisgarc06@outlook.com>
2025-06-15 13:05:31 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+firstH1 = document.querySelector("h1");
+firstH1.addEventListener("click", () => {
+  if (firstH1.classList.includes("yellow") firstH1.classList.remove("yellow");
+  else firstH1.classList.add("yellow");
+})
