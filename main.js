8208f5358c37afe1eef5916e93545f218ba86a88
meganasus <crisgarc06@outlook.com>
2025-06-21 21:47:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const firstH1 = document.querySelector("h1");
-firstH1.addEventListener("click", () => {
-  if (firstH1.classList.value.includes("yellow")) document.querySelector("h1").classList.remove("yellow");
-  else document.querySelector("h1").classList.add("yellow");
+const pageForm = document.querySelector("form.contact-us");
+pageForm.addEventListener(event => () {
+    event.preventDefault();
+    console.log("Hello, World");
 })
